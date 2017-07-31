949be0fb5e4582da9cd3a9d67ac3edb3d27334eb
nat <nat.foster@gmail.com>
2017-07-31 10:50:14 -0600

    basic example

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, render_template, request, redirect
 
 
 app = Flask(__name__)
@@ -7,3 +7,25 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, world! Welcome to the example app for external items in SEI!'
+
+
+@app.route('/get_url/<route>')
+def get_url(route):
+    return url_for(route)
+
+
+@app.route('/get_url/<route>', methods=['POST'])
+def get_url_by_post(route):
+    return url_for(route)
+
+
+@app.route('/thank_you')
+def thank_you():
+    return render_template('thank_you.html')
+
+
+@app.route('/everybody_wins', methods=['GET', 'POST'])
+def everybody_wins():
+    if request.method == 'POST':
+        return redirect(url_for('thank_you'))
+    return render_template('everybody_wins.html')
