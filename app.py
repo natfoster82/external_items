eba65dc12c546a5ce6a71eef8302e3b1a5f7c3e2
nat <nat.foster@gmail.com>
2017-07-31 15:45:54 -0600

    everybody wins item with server side response submission

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, url_for, render_template, request, redirect, jsonify
+from flask import Flask, url_for, render_template, request, redirect, jsonify, abort
+import requests
+from requests.auth import HTTPBasicAuth
 
 
+# app setup
 app = Flask(__name__)
 
 
+# views
 @app.route('/')
 def hello():
     return 'Hello, world! Welcome to the example app for external items in SEI!'
@@ -38,9 +42,20 @@ def thank_you():
 @app.route('/everybody_wins', methods=['GET', 'POST'])
 def everybody_wins():
     if request.method == 'POST':
-        request_id = request.args.get('request_id')
-        if request_id:
-            # call the SEI webhook
-            pass
-        return redirect(url_for('thank_you'))
+        response_id = request.args.get('response_id')
+        exam_id = request.args.get('exam_id')
+        if response_id and exam_id and exam_id in app.config['ROLE_SECRETS']:
+            url = app.config['SEI_URL_BASE'] + '/set_response/' + response_id
+            json = {
+                'value': request.form['submit']
+            }
+            headers = {
+                'x-sei-role-secret': app.config['ROLE_SECRETS'][exam_id]
+            }
+            username = app.config['SEI_ID']
+            password = app.config['SEI_SECRET']
+            r = requests.post(url, json=json, headers=headers, auth=HTTPBasicAuth(username=username, password=password))
+            if r.status_code not in {200, 201}:
+                abort(400)
+            return redirect(url_for('thank_you'))
     return render_template('everybody_wins.html')
--- config.py
+++ config.py
@@ -4,3 +4,7 @@ from json import loads
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'devkey')
 API_TOKEN = os.environ.get('AUTH_TOKEN', 'devtoken')
+SEI_URL_BASE = os.environ.get('SEI_URL_BASE', 'https://sei.caveon.com')
+SEI_ID = os.environ.get('SEI_ID')
+SEI_SECRET = os.environ.get('SEI_SECRET')
+ROLE_SECRETS = loads(os.environ.get('ROLE_SECRETS', '{}'))
