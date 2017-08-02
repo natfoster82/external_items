67868b4bbcce3610ab077f1e8df9d2232216b159
nat <nat.foster@gmail.com>
2017-08-02 15:51:50 -0600

    everybody wins

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ def hello():
 
 @app.route('/get_url/<route>')
 def get_url(route):
-    return url_for(route, **request.args, _external=True)
+    return url_for(route, _external=True, **request.args)
 
 
 @app.route('/get_url/<route>', methods=['POST'])
 def get_url_by_post(route):
     data = request.get_json()
-    return url_for(route, **data, _external=True)
+    return url_for(route, _external=True, **data)
 
 
 @app.route('/get_url_secure/<route>')
@@ -32,7 +32,7 @@ def get_url_secure(route):
     if given_api_token != needed_api_token:
         return jsonify(), 404
     request.args.pop('api_token')
-    return url_for(route, **request.args, _external=True)
+    return url_for(route, _external=True, **request.args)
 
 
 @app.route('/thank_you')
@@ -44,19 +44,26 @@ def thank_you():
 def everybody_wins():
     if request.method == 'POST':
         response_id = request.args.get('response_id')
-        exam_id = request.args.get('exam_id')
-        if response_id and exam_id and exam_id in app.config['ROLE_SECRETS']:
+        if response_id:
+            exam_id = request.args.get('exam_id')
+            external_token = request.args.get('external_token')
             url = app.config['SEI_URL_BASE'] + '/set_response/' + response_id
             json = {
                 'value': request.form['submit']
             }
-            headers = {
-                'x-sei-role-secret': app.config['ROLE_SECRETS'][exam_id]
-            }
-            username = app.config['SEI_ID']
-            password = app.config['SEI_SECRET']
-            r = requests.post(url, json=json, headers=headers, auth=HTTPBasicAuth(username=username, password=password))
-            if r.status_code not in {200, 201}:
+            if exam_id and exam_id in app.config['ROLE_SECRETS']:
+                headers = {
+                    'x-sei-role-secret': app.config['ROLE_SECRETS'][exam_id]
+                }
+                username = app.config['SEI_ID']
+                password = app.config['SEI_SECRET']
+                r = requests.post(url, json=json, headers=headers, auth=HTTPBasicAuth(username=username, password=password))
+            elif external_token:
+                url += '?external_token=' + external_token
+                r = requests.post(url, json=json)
+            else:
+                r = None
+            if r and r.status_code not in {200, 201}:
                 abort(400)
         return redirect(url_for('thank_you'))
-    return render_template('everybody_wins.html')
+    return render_template('everybody_wins.html', query_params=request.args)
